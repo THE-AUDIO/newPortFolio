73823cee81b0bff663c0e5a1dc4a88c1019fb77d
THE-AUDIO <nandraina.dev22@gmail.com>
2024-10-01 19:17:30 +0300

    v1.0.2

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 // ajout de la classe activr quand un lien a été acitvé
+window.addEventListener('DOMContentLoaded',()=>{
+    window.addEventListener('scroll', ()=>{
+        window.onscroll = () => {   
+             sections.forEach(section => {
+                 let scrollPosition = window.scrollY;
+                 let offset = section.offsetTop -150;
+                 
+                 let height = section.offsetHeight;// hauteur total de la secion avec la bordure        
+
+                 let id = section.getAttribute('id');
+                 if (scrollPosition >= offset && scrollPosition < offset + height) {
+                     // La section est visible à l'écran
+                     // Ajoutez une classe active au lien de navigation correspondant
+
+                     navLinks.forEach(link => {
+                         if (link.getAttribute('href') === `#${id}`) {
+                             link.classList.add('active');
+                         } else {
+                             link.classList.remove('active');
+                         }
+                     });
+                 }
+             });
+             /*=================== sticky navBarre =================*/
+             let header = document.querySelector('header')
+             header.classList.toggle('sticky',window.scrollY>100 )
+         }
+         
+    })
+})
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-   
-
-    sections.forEach(section => {
-        let scrollPosition = window.scrollY;
-        
-        let offset = section.offsetTop -150;
-        
-        let height = section.offsetHeight;// hauteur total de la secion avec la bordure        
-        // console.log(`scrol = ${scrollPosition} <br> hauteur ${height} <br> offset ${offset}`);
-
-
-        let id = section.getAttribute('id');
-
-        if (scrollPosition >= offset && scrollPosition < offset + height) {
-            // La section est visible à l'écran
-            // Ajoutez une classe active au lien de navigation correspondant
-            navLinks.forEach(link => {
-                if (link.getAttribute('href') === `#${id}`) {
-                    link.classList.add('active');
-                } else {
-                    link.classList.remove('active');
-                }
-            });
-        }
-    });
-    /*=================== sticky navBarre =================*/
-    let header = document.querySelector('header')
-    header.classList.toggle('sticky',window.scrollY>100 )
-}
-
-
-
+const btnContact = document.querySelector(".link-contact")
+const contactSide = document.querySelector(".new-contact")
+const btnColsed = document.querySelector(".closed");
     let iconActive = document.querySelector('#menu-icon')
     iconActive.addEventListener('click',()=>{
         iconActive.classList.toggle('bx-x')
@@ -54,4 +53,12 @@ elt.forEach((one)=>{
     })
 })
 
+btnContact.addEventListener("click",()=>{
+    contactSide.classList.add("new-contact-active")
+    document.body.classList.add("no-scrolling")
+})
+btnColsed.addEventListener("click",()=>{
+    contactSide.classList.remove("new-contact-active")
+    document.body.classList.remove("no-scrolling")
+})
 // =============================== Send Email =========================
